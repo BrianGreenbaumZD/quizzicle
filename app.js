80fe68cce46076d8e65ccaba8bd703b3cbe3004b
Brian Greenbaum <bgreenbaum@zendesk.com>
2013-11-13 10:49:24 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+(function() {
+
+  return {
+//    BASE_URL: "http://localhost:8000",
+    BASE_URL: "https://voice-tester-service.herokuapp.com",
+
+    requests: {
+      answerCall: function() {
+        return {
+          url: "%@/answer".fmt(this.BASE_URL)
+        };
+      },
+      denyCall: function() {
+        return {
+          url: "%@/deny".fmt(this.BASE_URL)
+        };
+      }
+    },
+
+    events: {
+      'app.activated': 'onActivation',
+      'click .answer': 'onAnswerClick',
+      'click .deny':   'onDenyClick',
+      'notification.incoming_call': 'handleCall'
+    },
+
+    handleCall: function(data) {
+      console.log('incoming call');
+      console.log(data);
+      this.switchTo('call', { sid: data });
+      this.popover();
+    },
+
+    onActivation: function() {
+      console.log('Twilio Test App activated');
+      this.switchTo('call');
+    },
+
+    onAnswerClick: function(event) {
+      event.preventDefault();
+      console.log('Answering');
+      this.ajax('answerCall');
+    },
+
+    onDenyClick: function(event) {
+      event.preventDefault();
+      console.log('Denying');
+      this.ajax('denyCall');
+      this.switchTo('nocall');
+    }
+  };
+
+}());
